e0684682382dae2758ebd4e0e4e82cc866d48207
andrea marano <andreamarano2004@gmail.com>
2023-08-31 15:56:06 +0200

    webpack and emotion library removed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
 const nextConfig = {
     i18n: {
         locales: ["it"],
         defaultLocale: "it",
+    },
+    //Webpack bundle analyzer
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.plugins.push(new BundleAnalyzerPlugin({
+                analyzerMode: 'server',
+                analyzerPort: 8888,
+                openAnalyzer: true,
+            }))
+        }
+        return config
     }
 }
 
