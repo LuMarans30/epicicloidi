f158837b10205085ae8751107b3ecf956d3cf15e
andrea marano <andreamarano2004@gmail.com>
2023-09-08 00:33:19 +0200

    no animations in production

--- src/app/components/Routes.tsx
+++ src/app/components/Routes.tsx
@@ -19,59 +19,64 @@ export const RoutesPages = [
         label: "Cardioide",
         description: "Scopri come disegnare una cardioide con questo algoritmo.",
         path: "/cardioide-con-algoritmo",
-        image: Images[1]
+        image: Images[1],
+        canvasElement: <></>
     },
     {
         label: "Cicloide",
         description: "Scopri come è fatta una cicloide e cosa succede se il punto che la descrive è all'interno o all'esterno del cerchio.",
         path: "/cicloide-con-punto-variabile",
-        image: Images[2]
+        image: Images[2],
+        canvasElement: <></>
     },
     {
         label: "Penna geometrica",
         description: "Fai ruotare un pianeta e scopri la sua traiettoria.",
         path: "/penna-geometrica-suardi",
-        image: Images[3]
+        image: Images[3],
+        canvasElement: <></>
     },
     {
         label: "Cerchio traslato",
         description: "Abbiamo riprodotto con GeoGebra come Suardi ha disegnato un cerchio traslato del raggio dell'epiciclo.",
         path: "/cerchio-traslato",
-        image: Images[4]
+        image: Images[4],
+        canvasElement: <></>
     },
     {
         label: "3 punti allineati",
         description: "Scopri come disegnare 3 punti allineati con la Penna Geometrica.",
         path: "/dimostrazione-punti-allineati",
-        image: Images[5]
+        image: Images[5],
+        canvasElement: <></>
     },
     {
         label: "Spirale di Archimede",
         description: "Abbiamo riprodotto con GeoGebra la spirale di Archimede con la macchina di Suardi.",
         path: "/spirale-archimedea",
         image: Images[6],
-        canvasElement: <SpiralAnimation />
+        canvasElement: <></>
     },
     {
         label: "Epicicloide",
         description: "Scopri come disegnare le epicicloidi facendo variare il raggio del cerchio tangente.",
         path: "/epicicloide-al-variare-raggio",
         image: Images[7],
-        canvasElement: <HypocycloidCanvas />
+        canvasElement: <></>
     },
     {
         label: "Epitrocoidi",
         description: "Scopri come disegnare le epitrocoidi.",
         path: "/epitrocoidi",
         image: Images[8],
-        canvasElement: <EpitrochoidCanvas />
+        canvasElement: <></>
     },
     {
         label: "Ipotrocoidi",
         description: "Scopri come disegnare le ipotrocoidi.",
         path: "/ipotrocoidi",
         image: Images[9],
-        canvasElement: <HypotrochoidCanvas />
+        canvasElement: <></>
     }
 ]
 
